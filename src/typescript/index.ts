28c642c71bd3dcfe4e8f2272080646c1af99b52a
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-04-20 23:10:06 +0530

    Abstraction

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,1 +1,42 @@
-console.log('Hello world!');
+interface Shape {
+  area(): number;
+  perimeter(): number;
+}
+
+class Circle implements Shape {
+  constructor(private radius: number) {}
+
+  area(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+
+  perimeter(): number {
+    return 2 * Math.PI * this.radius;
+  }
+}
+
+class Rectangle implements Shape {
+  constructor(
+    private width: number,
+    private height: number,
+  ) {}
+
+  area(): number {
+    return this.width * this.height;
+  }
+
+  perimeter(): number {
+    return 2 * (this.width + this.height);
+  }
+}
+
+function calculateTotalArea(shape: Shape): number {
+  return shape.area();
+}
+
+// Client code
+const circle: Circle = new Circle(5);
+const rectangle: Rectangle = new Rectangle(4, 6);
+
+console.log(`Circle Area: ${calculateTotalArea(circle)}`);
+console.log(`Rectangle Area: ${calculateTotalArea(rectangle)}`);
